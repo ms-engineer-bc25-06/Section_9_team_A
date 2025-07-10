df2aaaedd1da621e5fdb136c759f3e81631a291f
RuiOomura <asxnr2525@gmail.com>
2025-07-10 14:57:07 +0900

    add: バリデーション

--- RuiOomura_section_6/frontend/app/signup/page.tsx
+++ RuiOomura_section_6/frontend/app/signup/page.tsx
@@ -2,44 +2,56 @@
 "use client";
 
 import React from 'react';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
 console.log(auth);
 
-export default function SignUpPage() {
-    const [email, setEmail] = useState(""); // メールアドレスの状態
-    const [password, setPassword] = useState(""); // パスワードの状態
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault(); // ページのリロードを防ぐ
-        console.log("メール:", email);
-        console.log("パスワード", password);
+type FormData = {
+    email: string;
+    password: string;
+};
 
+export default function SignUpPage() {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    const onSubmit = async (data: FormData) => {
+        // TODO: console.log消す
+        console.log("メール:", data.email);
+        console.log("パスワード", data.password);
+
+        // サーバーに送信する処理
         try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.password);
             console.log("ユーザー登録成功:", userCredential.user);
         } catch(error) {
             console.error("ユーザー登録失敗:", error);
         };
     };
 
-
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-md mx-auto mt-20">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 max-w-md mx-auto mt-20">
             <input
                 type="email"
                 placeholder="email@abc.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                {...register('email', { required: "メールアドレスは必須です" })}
                 className="p-2 border rounded"     
             />
+
+            {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
+
             <input
                 type="password"
                 placeholder="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="p-2 border rounded"
+                {...register('password', { required: "パスワードは必須です", minLength: {
+                    value: 6,
+                    message: "パスワードは6文字以上にしてください"
+                }
+             })}
+             className="p-2 border rounded"
             />
+
+            {errors.password && <p className="test-red-500 text-sm">{errors.password.message}</p>}
+
             <button type="submit" className="bg-blue-500 text-white p-2 rounded">
                 Sign Up
             </button>  
