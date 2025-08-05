1dd12efbb1e251e948d7732c2afd97cf781ae9c2
shizuka0831 <shizuka.matsuura0831@gmail.com>
2025-08-05 10:35:27 +0900

    Delete RuiOomura_section_6 directory

--- RuiOomura_section_6/backend/src/controllers/usercontroller.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-export const getUsers = async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-};
